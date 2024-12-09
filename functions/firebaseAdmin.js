c56d6152ac80d9f6fded38ffb74b13d45723fda7
daynerjc <daynercespedes4@gmail.com>
2024-12-09 02:47:02 -0500

    feat: Add support for profile creation

---
+++ functions/firebaseAdmin.js
@@ -0,0 +1,27 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: "your-project-id.appspot.com",
+});
+
+if (process.env.FIREBASE_EMULATOR_HOST) {
+  const {auth, firestore, storage} = admin;
+
+  const emulatorHost = "127.0.0.1";
+  const authPort = 9099;
+  const firestorePort = 8080;
+  const storagePort = 9199;
+
+  auth().useEmulator(`http://${emulatorHost}:${authPort}`);
+
+  firestore().settings({
+    host: `${emulatorHost}:${firestorePort}`,
+    ssl: false,
+  });
+
+  storage().useEmulator(emulatorHost, storagePort);
+}
+
+module.exports = admin;
--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,65 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+/* eslint-disable max-len */
+const functions = require("firebase-functions");
+const express = require("express");
+const admin = require("./firebaseAdmin");
+const {FieldValue} = require("firebase-admin/firestore");
+const mime = require("mime-types");
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const app = express();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+app.use(express.json());
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+app.post("/create-profile", async (req, res) => {
+  console.log("Request body:", req.body);
+  const {name, email, password, photo} = req.body;
+
+  if (!name || !email || !password || !photo) {
+    return res.status(400).send("Name, email, photo and password are required.");
+  }
+
+  try {
+    const userRecord = await admin.auth().createUser({
+      email,
+      password,
+      displayName: name,
+    });
+
+    let photoURL = null;
+    if (photo) {
+      console.log("Received photo (Base64):", photo);
+      const base64Data = photo.replace(/^data:image\/\w+;base64,/, "");
+      const buffer = Buffer.from(base64Data, "base64");
+
+      const mimeType = mime.lookup(photo);
+      if (!mimeType) {
+        return res.status(400).send("Invalid image format.");
+      }
+
+      const fileExtension = mime.extension(mimeType);
+      const filePath = `profilePictures/${userRecord.uid}.${fileExtension}`;
+
+      const file = admin.storage().bucket().file(filePath);
+      await file.save(buffer, {
+        contentType: mimeType,
+      });
+
+      const emulatorHost = `http://${process.env.FIREBASE_STORAGE_EMULATOR_HOST}`;
+      photoURL = `${emulatorHost}/v0/b/${file.bucket.name}/o/
+      ${encodeURIComponent(filePath)}?alt=media`;
+    }
+
+    await admin.firestore().collection("users").doc(userRecord.uid).set({
+      name,
+      email,
+      photoURL,
+      createdAt: FieldValue.serverTimestamp(),
+    });
+
+    res.status(200).send({success: true, uid: userRecord.uid, photoURL});
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).send({success: false, message: error.message});
+  }
+});
+
+exports.api = functions.https.onRequest(app);
