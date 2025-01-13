d2a0ffe95f89f24f76b5baaae4125600f1622f34
daynerjc <daynercespedes4@gmail.com>
2025-01-13 13:27:50 -0500

    feat(auth): Add API support for Sign In with email(2).

---
+++ functions/src/auth/login/login_email.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable new-cap */
+const express = require("express");
+const router = express.Router();
+const admin = require("firebase-admin");
+
+router.post("/auth/login/email", async (req, res) => {
+  const {token} = req.body;
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    const uid = decodedToken.uid;
+
+    const userDoc = await admin.firestore().collection("users")
+        .doc(uid).get();
+
+    if (!userDoc.exists) {
+      return res.status(404)
+          .json({success: false, message: "User not found"});
+    }
+
+    const userData = userDoc.data();
+    console.log("Login Successful: ", userData);
+    res.status(200).json({success: true, user: userData});
+  } catch (error) {
+    console.error("Token verification failed:", error);
+    res.status(500)
+        .json({success: false, message: "Token verification failed"});
+  }
+});
+
+module.exports = router;
