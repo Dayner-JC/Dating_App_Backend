d2be612d014094201fe300b8011cf38ff8e03efc
daynerjc <daynercespedes4@gmail.com>
2025-01-30 02:07:18 -0500

    Update Firebase Admin.

--- functions/src/utils/firebaseAdmin.js
+++ functions/src/utils/firebaseAdmin.js
@@ -7,22 +7,22 @@ admin.initializeApp({
 });
 
 
-// if (process.env.FIREBASE_EMULATOR_HOST) {
-//   const {auth, firestore, storage} = admin;
+if (process.env.FIREBASE_EMULATOR_HOST) {
+  const {auth, firestore, storage} = admin;
 
-//   const emulatorHost = "0.0.0.0";
-//   const authPort = 9099;
-//   const firestorePort = 8080;
-//   const storagePort = 9199;
+  const emulatorHost = "0.0.0.0";
+  const authPort = 9099;
+  const firestorePort = 8080;
+  const storagePort = 9199;
 
-//   auth().useEmulator(`http://${emulatorHost}:${authPort}`);
+  auth().useEmulator(`http://${emulatorHost}:${authPort}`);
 
-//   firestore().settings({
-//     host: `${emulatorHost}:${firestorePort}`,
-//     ssl: false,
-//   });
+  firestore().settings({
+    host: `${emulatorHost}:${firestorePort}`,
+    ssl: false,
+  });
 
-//   storage().useEmulator(emulatorHost, storagePort);
-// }
+  storage().useEmulator(emulatorHost, storagePort);
+}
 
 module.exports = admin;
